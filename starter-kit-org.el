7950c6639dbd7b442f3f5d7c05f1c9e3edd83c83
Ben Lowery <ben@blowery.org>
2009-04-29 15:50:20 -0400

    pull in anything and org-jira

--- starter-kit-org.el
+++ starter-kit-org.el
@@ -1,5 +1,6 @@
 (require 'org-install)
- ;; The following lines are always needed.  Choose your own keys.
+(require 'org-jira)
+;; The following lines are always needed.  Choose your own keys.
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
